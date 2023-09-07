10eae7de04d666fbf6f813262d2a212feb1d9f8e
julia-yao <119920098+julia-yao@users.noreply.github.com>
2023-09-07 15:34:43 +0800

    Status Check

--- Async/async.js
+++ Async/async.js
@@ -1,13 +1,14 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-    //console.log(request,request.readyState); //顯示狀態4 
-    if(request.readyState === 4){
-        console.log(request.responseText); //印出json data的資料 
+    //console.log(request,request.readyState); //條件檢查顯示狀態4 和檢查端點狀態是 Successful responses 200
+    if(request.readyState === 4 && request.status === 200){
+        console.log(request, request.responseText); //印出json data的資料 
+    } else if(request.readyState === 4) {  
+        console.log( 'could not fetch the data');
     }
-})
-
+});
 //設定請求
-request.open('GET','https://jsonplaceholder.typicode.com/todos/')
+request.open('GET','https://jsonplaceholder.typicode.com/todoss/')
 //發送請求
 request.send();
