b4f39e84e5d33b37a4076b25f8cf2b95bc374375
julia-yao <119920098+julia-yao@users.noreply.github.com>
2023-09-07 18:06:54 +0800

    Callback Functions

--- Async/async.js
+++ Async/async.js
@@ -1,14 +1,32 @@
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-    //console.log(request,request.readyState); //條件檢查顯示狀態4 和檢查端點狀態是 Successful responses 200
-    if(request.readyState === 4 && request.status === 200){
-        console.log(request, request.responseText); //印出json data的資料 
-    } else if(request.readyState === 4) {  
-        console.log( 'could not fetch the data');
+    request.addEventListener('readystatechange', () => {
+        //console.log(request,request.readyState); //條件檢查顯示狀態4 和檢查端點狀態是 Successful responses 200
+        if(request.readyState === 4 && request.status === 200){
+            callback( undefined, request.responseText);
+        } else if(request.readyState === 4) {  
+            callback('could not fetch data', undefined);
+        }
+    });
+
+    //設定請求
+    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+}
+
+console.log(1);
+console.log(2);
+ 
+//用callback 傳入函數
+getTodos((err,data)=>{
+    console.log('callback fired');
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
     }
 });
-//設定請求
-request.open('GET','https://jsonplaceholder.typicode.com/todoss/')
-//發送請求
-request.send();
+
+console.log(3);
+console.log(4);
