989ddf9fa634243f6080d0d3f98be59c3dbeba6c
julia-yao <119920098+julia-yao@users.noreply.github.com>
2023-09-07 18:58:31 +0800

    test callback hell

--- Async/async.js
+++ Async/async.js
@@ -1,32 +1,26 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange', () => {
-        //console.log(request,request.readyState); //條件檢查顯示狀態4 和檢查端點狀態是 Successful responses 200
+
         if(request.readyState === 4 && request.status === 200){
-            callback( undefined, request.responseText);
+            const data = JSON.parse(request.responseText);
+            callback( undefined, data );
         } else if(request.readyState === 4) {  
             callback('could not fetch data', undefined);
         }
     });
 
     //設定請求
-    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET',resource );
     request.send();
 }
 
-console.log(1);
-console.log(2);
- 
 //用callback 傳入函數
-getTodos((err,data)=>{
-    console.log('callback fired');
-    if(err){
-        console.log(err);
-    } else {
+getTodos('todos/luigi.json', (err,data)=>{
+    console.log(data);
+    getTodos('todos/mario.json', (err,data)=>{
         console.log(data);
-    }
+        
+    });
 });
-
-console.log(3);
-console.log(4);
